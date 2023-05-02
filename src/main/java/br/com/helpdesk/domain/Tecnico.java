78b1ce4f69baa4a7431c9fd936000bb424776c85
WilliamFilho <williamfilho20@gmail.com>
2023-05-02 08:31:03 -0300

    Classes de dominio iniciadas

---
+++ src/main/java/br/com/helpdesk/domain/Tecnico.java
@@ -0,0 +1,24 @@
+package br.com.helpdesk.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tecnico extends Pessoa{
+    private List<Chamado> chamados = new ArrayList<>();
+
+    public Tecnico(List<Chamado> chamados) {
+        super();
+    }
+
+    public Tecnico(Integer id, String nome, String cpf, String email, String senha, List<Chamado> chamados) {
+        super(id, nome, cpf, email, senha);
+    }
+
+    public List<Chamado> getChamados() {
+        return chamados;
+    }
+
+    public void setChamados(List<Chamado> chamados) {
+        this.chamados = chamados;
+    }
+}
